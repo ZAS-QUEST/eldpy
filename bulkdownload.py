0e799a189f8c2be108b301979ff0df34bb73659a
glottotopia <sebastian.nordhoff@glottotopia.de>
2020-04-20 19:27:48 +0200

    accommodate for different file types

--- bulkdownload.py
+++ bulkdownload.py
@@ -23,7 +23,7 @@ def download_file(url, filename):
                             filename=filename,
                             reporthook=t.update_to)
 
-def elar_eaf_download(filename, phpsessid):
+def elar_eaf_download(filename, phpsessid, extension):
     # check for validity of ID
     try:
         soasID = filename.split("oai:soas.ac.uk:")[1]
@@ -33,7 +33,7 @@ def elar_eaf_download(filename, phpsessid):
     # prepare request
     url = "https://elar.soas.ac.uk/Record/%s" % soasID
     cookie = {"PHPSESSID": phpsessid}
-    print(url)
+    print("checking", url)
     # retrieve catalog page
     with requests.Session() as s:
         # r = s.post(url, cookies=cookie, data=payload)
@@ -42,35 +42,36 @@ def elar_eaf_download(filename, phpsessid):
         # extract links to ELAN files
         try:
             links = fromstring(html).findall(".//tbody/tr/td/a")
-            eaflocations = {a.attrib["href"]
+            locations = {a.attrib["href"]
                             for a in links
-                            if a.attrib["href"].endswith("eaf")
+                            if a.attrib["href"].endswith(extension)
                             }
         except AttributeError:#not an ELAN file
-            print("ELAN files are not accessible")
+            print("files are not accessible")
             return
         # dowload identified files
         retrievedfiles = []
-        for eaflocation in eaflocations:
-            print(eaflocation)
-            eafname = eaflocation.split("/")[-1]
-            print("  downloading %s:" % eafname)
-            eafname = "./downloads/elar/%s.eaf" % eafname[:200]  # avoid overlong file names
-            eafname = "./%s.eaf" % eafname[:200]  # avoid overlong file names
-            r2 = s.post(eaflocation, cookies=cookie)
-            eafcontent = r2.text
+        for location in locations:
+            print(location)
+            filename = location.split("/")[-1]
+            print("  downloading %s:" % filename)
+            #filename = "./downloads/elar/%s.eaf" % filename[:200]  # avoid overlong file names
+            filename = "./%s.%s" % (filename[:200], extension)  # avoid overlong file names
+            r2 = s.post(location, cookies=cookie)
+            content = r2.text
             #retrievedfiles.append({eafname: eafcontent})
-            with open(eafname, 'w') as out:
-                out.write(eafcontent)
+            with open(filename, 'w') as out:
+                out.write(content)
 
 
 
 filetypes = {
-    "ELAN":"text/x-eaf+xml",
-    "Toolbox":"text/x-toolbox-text",
-    "transcriber":"text/x-trs",
-    "praat":"text/praat-textgrid",
-    "Flex":"FLEx"
+    1: ("ELAN","text/x-eaf+xml","eaf"),
+    2: ("Toolbox","text/x-toolbox-text","tbx"),
+    3: ("transcriber","text/x-trs","trs"),
+    4: ("praat","text/praat-textgrid","textgrid"),
+    5: ("Flex","FLEx","xml"),
+    6: ("Wave audio","audio/x-wav","wav")
     }
 
 archives = {
@@ -82,17 +83,27 @@ archives = {
     }
 
 
+print("Which files are you interested in?")
+for i in filetypes:
+    print("%i) %s" % (i, filetypes[i][0]))
+input_given = False
+while input_given == False:
+    try:
+        filetypeinput = int(input("Select number and hit enter"))
+        input_given = True
+    except ValueError:
+        pass
 
+typename, mimetype, extension = filetypes[filetypeinput]
+print("You have chosen %s (%s)" % (typename, extension))
 
-#try:
-    #olacdump = open('ListRecords.xml')
-#except FileNotFoundError:
-    #print("no olac dump found. Retrieving dump from http://www.language-archives.org/xmldump/ListRecords.xml.gz")
-
-
-typ = filetypes["ELAN"]
-
-print("unpacking zipped OLAC file")
+try:
+    print("unpacking zipped OLAC file")
+    gunzipped_file = gzip.open("ListRecords.xml.gz")
+except FileNotFoundError:
+    print("no olac dump found. Retrieving dump from http://www.language-archives.org/xmldump/ListRecords.xml.gz")
+    download_file('http://www.language-archives.org/xmldump/ListRecords.xml.gz', 'ListRecords.xml.gz')
+    print("unpacking zipped OLAC file")
 gunzipped_file = gzip.open("ListRecords.xml.gz")
 print("parsing OLAC file")
 tree = etree.parse(gunzipped_file)
@@ -100,8 +111,9 @@ tree = etree.parse(gunzipped_file)
 globalidentifiers = {}
 #retrieve all records which references files of interest
 dcformats = tree.findall(".//{http://purl.org/dc/elements/1.1/}format")
+
 for dcformat in dcformats:
-    if dcformat.text.strip() == typ:
+    if dcformat.text.strip() == mimetype:
         identifiers = dcformat.getparent().findall(
             ".//{http://purl.org/dc/elements/1.1/}identifier"
         )
@@ -112,29 +124,22 @@ for dcformat in dcformats:
 
 print("found %i relevant records" % len(globalidentifiers))
 
-subset = list(globalidentifiers.keys())[:50]
-print(subset)
+limit = 9999999
+subset = list(globalidentifiers.keys())[:limit]
+
+print("preparing to download %i files" % len(subset))
+#print(subset)
 
 #retrieve links
 login_url = "https://elar.soas.ac.uk/MyResearch/UserLogin"
 session = requests.Session()
 un_name = "username"
 pw_name = "password"
-print("enter user name:")
-username = "%s"%input()
-print("enter password:")
-password = "%s"%input()
+username = input("enter user name for ELAR: \n")
+password = input("Your password will only be used for this login session and not be stored anywhere. Enter password for ELAR: \n")
 values = {un_name: username.strip(), pw_name: password.strip()}
 r = session.post(login_url, data = values)
 phpsessid = session.cookies.get_dict().get('PHPSESSID')
 
 for globalidentifier in subset:
-    elar_eaf_download(globalidentifier, phpsessid)
-
-#enumerate
-
-#download 10.
-
-#Estimate duration
-
-#finish
+    elar_eaf_download(globalidentifier, phpsessid, extension)
