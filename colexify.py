522e45596a9378273a8bf545f7121f716a4ef123
glottotopia <sebastian.nordhoff@glottotopia.de>
2023-10-09 16:18:25 +0200

    reformat with black

--- colexify.py
+++ colexify.py
@@ -19,14 +19,14 @@ def analyze_colexifications(directory, threshold=3, double_files=[]):
     print(f"found {len(json_files)} json files")
     for fn in json_files:
         print(f"\nreading {fn}")
-        j = json.loads(open(fn).read())
-        for a in j: #there is only one archive in the json file
-            oldskip = None
-            for eaf in j[a]:
-                dico = defaultdict(dict)
-                collection = eaf.split('/')[1]
-                if collection.startswith("1839"):
-                   continue
+        json_content = json.loads(open(fn).read())
+        for archive in json_content:  # there is only one archive in the json file
+            oldskip = None  # we only give skipping information once per skipped file
+            for eaf in json_content[archive]:
+                dico = defaultdict(dict)  # we store (word,gloss) tuples
+                collection = eaf.split("/")[1]
+                if collection.startswith("1839"):  # many spurious files start with 1839
+                    continue
                 if collection in double_files:
                     if oldskip is None:
                         print()
@@ -37,53 +37,105 @@ def analyze_colexifications(directory, threshold=3, double_files=[]):
                         oldskip = collection
                     continue
                 oldskip = None
-                for tiertype in j[a][eaf]:
-                    for tiername in j[a][eaf][tiertype]:
-                        for ancestor in j[a][eaf][tiertype][tiername]:
+                # drill down to the relevant information in the ELAN tree
+                for tiertype in json_content[archive][eaf]:
+                    for tiername in json_content[archive][eaf][tiertype]:
+                        for ancestor in json_content[archive][eaf][tiertype][tiername]:
                             for key in ancestor:
                                 for word_gloss_tuple in ancestor[key]:
-                                    #print(word_gloss_tuple,len(word_gloss_tuple))
-                                    word,gloss = word_gloss_tuple
+                                    word, gloss = word_gloss_tuple
                                     try:
                                         gloss = gloss.lower().strip()
                                     except AttributeError:
                                         continue
+                                    # we exclude all glosses which are not in plain language
                                     nonascii = False
                                     for character in gloss:
-                                        if ord(character)>256:
-                                            nonascii = True #all glosses should be in ascii. If something is not ascii, it is probably not relevant for colexification analysis
+                                        if ord(character) > 256:
+                                            nonascii = True  # all glosses should be in ascii. If something is not ascii, it is probably not relevant for colexification analysis
                                             continue
                                     if nonascii:
                                         continue
                                     flag = False
-                                    for c in " 123*АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЪъЫыЬьЭэЮюЯяҷөғɨβŋñ?":
+                                    for c in " 123*?":
                                         if c in gloss:
                                             flag = True
                                     if flag:
                                         continue
-                                    if gloss in ("stem", "suffix", "prefix", "n", "v", "nprop", "adj","pron","adv", "q", "mrph", "cl", "interj", "intr", "stat", "rus", "cardnum", "ordnum", "coordconn", "adp", "root", "seq", "attr", "exi.neg", "verb","adj>adv", "vd", "num>n", "adj>adv","postp","conj","enclitic","num", "prep","pro","-", "conn"):
+                                    # we skip glosses which are not translations but rather categorizations
+                                    if gloss in (
+                                        "stem",
+                                        "suffix",
+                                        "prefix",
+                                        "n",
+                                        "v",
+                                        "nprop",
+                                        "adj",
+                                        "pron",
+                                        "adv",
+                                        "q",
+                                        "mrph",
+                                        "cl",
+                                        "interj",
+                                        "intr",
+                                        "stat",
+                                        "rus",
+                                        "cardnum",
+                                        "ordnum",
+                                        "coordconn",
+                                        "adp",
+                                        "root",
+                                        "seq",
+                                        "attr",
+                                        "exi.neg",
+                                        "verb",
+                                        "adj>adv",
+                                        "vd",
+                                        "num>n",
+                                        "adj>adv",
+                                        "postp",
+                                        "conj",
+                                        "enclitic",
+                                        "num",
+                                        "prep",
+                                        "pro",
+                                        "-",
+                                        "conn",
+                                    ):
                                         continue
-
+                                    # store the found information
                                     dico[word][gloss] = collection
+                # prepare the information for transfer
                 for word in dico:
-                    if len(dico[word])<2:
+                    if len(dico[word]) < 2:  # skip one letter words
                         continue
-                    meaninglist= [x for x in dico[word].keys() if x is not None]
+                    meaninglist = [x for x in dico[word].keys() if x is not None]
                     meaninglist.sort()
-                    for i, meaning in enumerate(meaninglist[:-1]):
-                        meaning1 = meaninglist[i]
-                        meaning2 = meaninglist[i+1]
-                        colexification_dic[(meaning1,meaning2)][collection] = True
+                    # transform the meaninglist to a list of 2-tuples
+                    for i, meaning_i in enumerate(meaninglist):
+                        if i == len(meaninglist):
+                            # avoid going beyond the end of the list with i+1
+                            break
+                        for j, meaning_j in enumerate(meaninglist[i + 1 :]):
+                            # copy the information to the main aggregation dictionary
+                            colexification_dic[(meaning_i, meaning_j)][
+                                collection
+                            ] = True
 
-    strippeddic = {key:colexification_dic[key] for key in colexification_dic if len(colexification_dic[key].keys())>=threshold}
+    strippeddic = {
+        key: colexification_dic[key]
+        for key in colexification_dic
+        if len(colexification_dic[key].keys()) >= threshold
+    }
     return strippeddic
 
 
 if __name__ == "__main__":
     directory = sys.argv[1]
-    dic = analyze_colexifications(directory,double_files=["268837","WEW2018","zauzou-li-0535"])
-    # pprint.pprint(dic)
-    sorted_list = sorted([(len(dic[x]),x) for x in dic.keys()],reverse=True)
+    dic = analyze_colexifications(
+        directory, threshold=4, double_files=["268837", "WEW2018", "zauzou-li-0535"]
+    )
+    sorted_list = sorted([(len(dic[x]), x) for x in dic.keys()], reverse=True)
     print(f"\nfound {len(sorted_list)} colexification candidates")
     print("writing to colexification.json")
     with open("colexification.json", "w") as out:
